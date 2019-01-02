09aec128e6363fc812cbfb9bc6e3f8efd925b1dc
Maynard Magallen <mmagallen_180000002192@uic.edu.ph>
2019-01-02 17:54:10 +0800

    features installation

--- server.sh
+++ server.sh
@@ -198,7 +198,7 @@ cd ddos-deflate-master
 rm -rf /root/ddos-deflate-master.zip
 
 # bannerrm /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/MMagallen/Debian7-OpenVpn-Autoscript/master/issues.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart

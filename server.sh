d931eaca79642865f12e99cae5def1b77519528d
Maynard Magallen <mmagallen_180000002192@uic.edu.ph>
2019-01-02 17:28:23 +0800

    client port 1194 added

--- server.sh
+++ server.sh
@@ -106,7 +106,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/MMagallen/Debian7-OpenVpn-Autoscript/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 

3863e2e7d9f8ef557fede21a98d71ff8c58e1d40
Maynard Magallen <mmagallen_180000002192@uic.edu.ph>
2019-01-02 17:20:10 +0800

    Port 1194 configuration add

--- server.sh
+++ server.sh
@@ -90,9 +90,9 @@ echo 'echo -e ""' >> .bashrc
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/MMagallen/Debian7-OpenVpn-Autoscript/master/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Setup by Clrkz</pre>" > /home/vps/public_html/index.html
+echo "<pre>Powered By: University of Immaculate Conception</pre>" > /home/vps/public_html/index.html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Clrkz/VPSAutoScrptz/master/vps.conf"
 service nginx restart
 

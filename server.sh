ee99dae151ccf01afab7a038a551f6f04d068b1f
Maynard Magallen <mmagallen_180000002192@uic.edu.ph>
2019-01-09 06:55:30 +0800

    Arrange

--- server.sh
+++ server.sh
@@ -149,37 +149,12 @@ sed -i $MYIP2 /etc/stunnel/stunnel.conf
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 service stunnel4 restart
 
-# install stunnel
-#apt-get install stunnel4 -y
-#cat > /etc/stunnel/stunnel.conf <<-END
-#cert = /etc/stunnel/stunnel.pem
-#client = no
-#socket = a:SO_REUSEADDR=1
-#socket = l:TCP_NODELAY=1
-#socket = r:TCP_NODELAY=1
-
-
-#[dropbear]
-#accept = 443
-#connect = 127.0.0.1:3128
-
-#END
-
-#Member certificate
-#openssl genrsa -out key.pem 2048
-#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
-#-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
-#cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
-
-#Configure stunnel
-#sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-#/etc/init.d/stunnel4 restart */
-###############################################################################
 # Install Ruby & lolcat
 apt-get -y install ruby
 gem install lolcat
 
-# install fail2banapt-get -y install fail2ban;
+# install
+fail2banapt-get -y install fail2ban;
 service fail2ban restart
 
 # install ddos deflate
